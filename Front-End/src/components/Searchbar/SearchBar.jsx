0b931941d5b4608b6412f199dc2c6dee602e8458
dev-adeel-arshad <adeelarshad.dev@gmail.com>
2026-06-21 00:03:21 +0500

    fix: correct case-sensitive CSS import in SearchBar component

--- Front-End/src/components/Searchbar/SearchBar.jsx
+++ Front-End/src/components/Searchbar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./SearchBar.css";
+import "./Searchbar.css";
 
 function SearchBar() {
   const [searchQuery, setSearchQuery] = useState("");
